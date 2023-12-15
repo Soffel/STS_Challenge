b23da63b0abf3a8b56bddd44e96fd0745e404b84
André Wagenknecht <Soffel@users.noreply.github.com>
2023-12-15 16:14:44 +0100

    Lösung :)

--- STS_Challenge/DateFinder.cs
+++ STS_Challenge/DateFinder.cs
@@ -1,9 +1,341 @@
-﻿namespace STS_Challenge;
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace STS_Challenge;
 
 public class DateFinder : IDateFinder
 {
-    public IEnumerable<(DateTime, DateUsage)> FindDates(IEnumerable<string> input)
+    public DateFinder() 
+    { 
+        year = 0;
+        date = DateTime.MinValue;
+        time = DateTime.MinValue;
+    }
+
+    private int year;
+    private DateTime date;
+    private DateTime time;
+
+    readonly CultureInfo[] cultures =
     {
-        return new []{(DateTime.Now, DateUsage.BDay)};
+        null,
+        CultureInfo.InvariantCulture,
+        CultureInfo.CurrentCulture,
+        CultureInfo.GetCultureInfo("de-DE"),
+    };
+
+    readonly string[] dateTimeFormats =
+    {
+        "dd.MM.yyyy HH:mm:ss",   
+        "dd.MM.yyyy HH:mm",      
+        "d.MM.yyyy HH:mm:ss",    
+        "d.MM.yyyy HH:mm",       
+        "dd.MM.yy HH:mm:ss",     
+        "dd.MM.yy HH:mm",        
+        "d.MM.yy HH:mm:ss",      
+        "d.MM.yy HH:mm",         
+        "dd.MMMM yyyy HH:mm:ss", 
+        "dd.MMMM yyyy HH:mm",    
+        "d.MMMM yyyy HH:mm:ss",  
+        "d.MMMM yyyy HH:mm",     
+        "dd.MMMM yy HH:mm:ss",   
+        "dd.MMMM yy HH:mm",      
+        "d.MMMM yy HH:mm:ss",    
+        "d.MMMM yy HH:mm",       
+
+        "yyyy-MM-ddTHH:mm:ss",
+        "yyyy-MM-ddTHH:mm",
+
+
+    };
+
+    readonly string[] dateFormats =
+    {
+        "dd.MM.yyyy",            
+        "d.MM.yyyy",             
+        "dd.MM.yy",              
+        "d.MM.yy",               
+        "dd.MMMM yyyy",            
+        "d.MMMM yyyy",           
+        "dd.MMMM yy",                
+        "d.MMMM yy",             
+
+        "yyyy-MM-dd",
+    };
+
+    readonly string[] dayFormats =
+    {
+        "dd.MM",           
+        "d.MM",            
+        "dd.MMM",          
+        "d.MMM",           
+        "dd.MMMM",         
+        "d.MMMM",          
+    };
+
+    readonly string[] timeFormats =
+    {
+        "HH:mm:ss",   
+        "HH:mm",      
+    };
+
+    private void Reset()
+    {
+        date = DateTime.MinValue;
+        time = DateTime.MinValue;
+    }
+
+    public IEnumerable<(DateTime, DateUsage)> FindDates(IEnumerable<string> inputs)
+    {
+        var listOfDates = new List<DateTime>();
+
+        foreach (string input in inputs)
+        {
+            foreach(string split in input.Split(' '))
+            {
+                if(isDateTime(split, out DateTime dt))
+                {                
+                    listOfDates.Add(dt);
+                    continue;          
+                }
+
+                if(IsDate(split, out DateTime c))
+                {          
+                    if(date != DateTime.MinValue && date.Year != DateTime.MinValue.Year)
+                    {
+                        listOfDates.Add(date);
+                        Reset();
+                    }
+
+                    date = new DateTime(c.Year, c.Month, c.Day);
+
+                    if (time != DateTime.MinValue && date.Year != DateTime.MinValue.Year)
+                    {
+                        time = new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
+                        listOfDates.Add(time);
+                        Reset();
+                    }
+                    continue;
+                }
+
+                if (IsYear(split, out int y))
+                {
+                    if(year == 0)
+                    { 
+                        year = y; 
+
+                        if(date != DateTime.MinValue && time != DateTime.MinValue)
+                        {
+                            date = new DateTime(year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
+                            listOfDates.Add(date);
+                            Reset();    
+                        }
+                        else
+                        {
+                            if (date != DateTime.MinValue)
+                            {
+                                date = new DateTime(year, date.Month, date.Day);
+                            }
+
+                            if (time != DateTime.MinValue)
+                            {
+                                time = new DateTime(year, time.Month, time.Day, time.Hour, time.Minute, time.Second);
+                            }
+                        }    
+                    }
+                    continue;
+                }
+                   
+
+                if (IsDayMonth(split, out DateTime d))
+                {
+                    if (date != DateTime.MinValue && date.Year != DateTime.MinValue.Year)
+                    {
+                        listOfDates.Add(date);
+                        Reset();
+                    }
+                    date = d;
+
+                    date = new DateTime(year != 0 ? year : DateTime.MinValue.Year, date.Month, date.Day);
+
+                    if (time != DateTime.MinValue && date.Year != DateTime.MinValue.Year)
+                    {
+                        time = new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
+                        listOfDates.Add(time);
+                        Reset();
+                    }
+                    
+                    continue;
+                }
+
+                if(IsTime(split, out DateTime t))
+                {
+                    time = t;
+                    if(date != DateTime.MinValue && date.Year != DateTime.MinValue.Year)
+                    {
+                        date = new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
+                        listOfDates.Add(date);
+                        Reset();       
+                    }
+                    continue;
+                }
+
+                var o = split;
+            }
+        }
+
+        if (date != DateTime.MinValue && date.Year != DateTime.MinValue.Year)
+        {
+            listOfDates.Add(date);
+            Reset();
+        }
+
+        listOfDates.Sort();
+
+        switch (listOfDates.Count)
+        {
+            case 1:
+            {
+                    yield return (listOfDates[0], DateUsage.ValidFrom);
+                    break;
+            }
+            case 2:
+            {
+                    yield return (listOfDates[0], DateUsage.ValidFrom);
+                    yield return (listOfDates[1], DateUsage.ValidTo);
+                    break;
+            }
+            case 3:
+            {
+                    yield return (listOfDates[0], IsBirthOrSale(listOfDates[0]));
+                    yield return (listOfDates[1], DateUsage.ValidFrom);
+                    yield return (listOfDates[2], DateUsage.ValidTo);
+                    
+                    break;
+            }
+            case 4:
+            {
+                    yield return (listOfDates[0], DateUsage.BDay);
+                    yield return (listOfDates[1], DateUsage.SaleDate);
+                    yield return (listOfDates[2], DateUsage.ValidFrom);
+                    yield return (listOfDates[3], DateUsage.ValidTo);
+                    break;
+            }
+            default:
+            {
+                    if (listOfDates.Count > 5)
+                    {
+                        yield return (listOfDates[^1], DateUsage.ValidTo);
+                        yield return (listOfDates[^2], DateUsage.ValidFrom);
+                        
+                        foreach (DateTime r in listOfDates.SkipLast(2))
+                        {
+                            yield return (r, DateUsage.DontKnow);
+                        }
+
+                        break;
+                    }
+                    else yield break; 
+            }
+        }
+    }
+
+    private static DateUsage IsBirthOrSale(DateTime date)
+    {
+        if ((DateTime.Now - date).TotalDays > 400) // evtl Vorverkauf 
+            return DateUsage.BDay;
+
+        return DateUsage.SaleDate;
+    }
+
+    private bool IsYear(string input, out int year)
+    {
+        year = 0;
+        foreach(var cultur in cultures)
+        {
+            if (DateTime.TryParseExact(input, "yyyy", cultur, DateTimeStyles.None, out DateTime parsedYear))
+            {
+
+                if (parsedYear.Year > DateTime.Now.AddYears(-100).Year &&
+                    parsedYear.Year < DateTime.Now.AddYears(2).Year)
+                {
+                    year = parsedYear.Year;
+                    return true;
+                }
+            }
+        }
+       
+        return false;    
+    }
+
+    private bool IsDayMonth(string input, out DateTime date)
+    {
+        date = DateTime.MinValue;
+
+        foreach (var cultur in cultures)
+            foreach (var format in dayFormats)
+            {
+                if (DateTime.TryParseExact(input, format, cultur, DateTimeStyles.None, out DateTime extractedDate))
+                {
+                   date = extractedDate;
+                    return true;
+                }
+            }
+
+
+        return false; 
+    }
+
+    private bool IsDate (string input, out DateTime date)
+    {
+        date = DateTime.MinValue;
+
+
+        foreach (var cultur in cultures)
+            foreach (var format in dateFormats)
+            {
+                if (DateTime.TryParseExact(input, format, cultur, DateTimeStyles.None, out DateTime extractedDate))
+                {
+                    date = extractedDate;
+                    return true;
+                }
+            } 
+       
+        return false;
+    }
+
+    private bool isDateTime(string input, out DateTime dateTime)
+    {
+        dateTime = DateTime.MinValue;
+
+        foreach(var cultur in cultures)
+            foreach (var format in dateTimeFormats)
+            {
+                if (DateTime.TryParseExact(input, format, cultur, DateTimeStyles.None, out DateTime extractedDate))
+                {
+                    dateTime = extractedDate;
+                    return true;
+                }
+            }
+        
+        return false;
+    }
+
+    private bool IsTime(string input, out DateTime time)
+    {
+        time = DateTime.MinValue;
+        foreach (var cultur in cultures)
+            foreach (var format in timeFormats)
+            {
+                if (DateTime.TryParseExact(input, format, cultur, DateTimeStyles.None, out DateTime extractedDate))
+                {
+                    time = extractedDate;
+                    return true;
+                }
+            } 
+       
+        return false; 
     }
 }
--- STS_Challenge_Tests/UnitTest1.cs
+++ STS_Challenge_Tests/UnitTest1.cs
@@ -1,29 +1,86 @@
 using System.Collections;
+using System.Diagnostics;
 using STS_Challenge;
+using Xunit.Abstractions;
 
 namespace STS_Challenge_Tests;
 
 public class UnitTest1
 {
+    private readonly ITestOutputHelper _log;
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        _log = testOutputHelper;
+    }
+
     [Theory]
     [ClassData(typeof(TestCases))]
-    public void Test1(IEnumerable<string> input, uint cOfDates, IEnumerable<(DateTime,DateUsage)> testData)
+    public void DateFinderTest(IEnumerable<string> input, uint cOfDates, IEnumerable<(DateTime, DateUsage)> testData)
     {
         Assert.Equal(cOfDates, (uint)testData.Count());
 
         IDateFinder finder = new DateFinder();
 
+        Stopwatch stopwatch = Stopwatch.StartNew();
         var result = finder.FindDates(input).ToList();
+        stopwatch.Stop();
+        _log.WriteLine("needs: " + stopwatch.ElapsedTicks);
 
         Assert.NotNull(result);
         Assert.NotEmpty(result);
 
-        Assert.Equal(cOfDates, (uint) result.Count);
+        if(cOfDates !=  (uint)result.Count)
+        {
+            foreach(var date in result)
+            {
+                _log.WriteLine(date.ToString());
+            }
+            Assert.Fail("Falsche anzhal!");
+        }
 
         foreach (var data in testData)
         {
             Assert.Contains(data, result);
         }
+    }
+
+
+    [Theory]
+    [ClassData(typeof(TestCases))]
+    public void SimpleDevTest(IEnumerable<string> input, uint cOfDates, IEnumerable<(DateTime, DateUsage)> testData)
+    {     
+        IDateFinder finder = new DateFinder();
+
+        var result = finder.FindDates(input).ToList();
+
+        var _ = result;
+
+        _log.WriteLine(result.Count.ToString());
+       
+    }
+
+    [Fact]
+    public void FormatTest()
+    {
+        string[] dayFormats =
+        {
+            "dd.MM",           
+            "d.MM",            
+            "dd.MMM",          
+            "d.MMM",           
+            "dd.MMMM",         
+            "d.MMMM",          
+        };
+
+        var date = new DateTime(2023, 10, 1);
+
+        foreach (var dayFormat in dayFormats)
+        {
+            
+            _log.WriteLine(dayFormat+ " >> " + date.ToString(dayFormat));
+        }
+
     }
 }
 
@@ -31,6 +88,7 @@ public class UnitTest1
 
 public class TestCases : IEnumerable<object[]>
 {
+    DateTime Now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
     public IEnumerator<object[]> GetEnumerator()
     {
         yield return new object[]
@@ -39,17 +97,17 @@ public class TestCases : IEnumerable<object[]>
             {
                 "kein Datum :o)",
                 "07.05.1999",
-                DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss"),
-                DateTime.Now.AddHours(3).ToString("yyyy-MM-ddTHH:mm:ss"),
-                DateTime.Now.AddMinutes(-5).ToString("yyyy-MM-ddTHH:mm:ss"),
+                Now.ToString("yyyy-MM-ddTHH:mm:ss"),
+                Now.AddHours(3).ToString("yyyy-MM-ddTHH:mm:ss"),
+                Now.AddMinutes(-5).ToString("yyyy-MM-ddTHH:mm:ss"),
             },
             4,
             new[]
             {
                 (new DateTime(1999,5,07), DateUsage.BDay),
-                (DateTime.Now, DateUsage.ValidFrom),
-                (DateTime.Now.AddHours(3),  DateUsage.ValidTo),
-                (DateTime.Now.AddMinutes(-5), DateUsage.SaleDate)
+                (Now, DateUsage.ValidFrom),
+                (Now.AddHours(3),  DateUsage.ValidTo),
+                (Now.AddMinutes(-5), DateUsage.SaleDate)
             }
         };
 
@@ -57,7 +115,7 @@ public class TestCases : IEnumerable<object[]>
         {
             new[]
             {
-                "2023-05-01 00:00:02-2023-06-01 03:00:00"
+                "2023-05-01 00:00:02 - 2023-06-01 03:00:00"
             },
             2,
             new[]
@@ -122,6 +180,58 @@ public class TestCases : IEnumerable<object[]>
                 (new DateTime(2023,6,1,3,0,0), DateUsage.ValidTo),
             }
         };
+        yield return new object[]
+        {
+            new[]
+            {
+                "1.Oktober 2023 00:00 Uhr bis 31.Oktober 23:59 Uhr",
+                "30.Sep 2023"
+            },
+            3,
+            new[]
+            {
+                (new DateTime(2023,10,1,0,0,0), DateUsage.ValidFrom),
+                (new DateTime(2023,10,31,23,59,0), DateUsage.ValidTo),
+                (new DateTime(2023,09,30,0,0,0), DateUsage.SaleDate),
+
+            }
+        };
+        yield return new object[]
+        {
+            new[]
+            {
+                "25.04.1991",
+                "1.Oktober 2023 00:00 Uhr bis 31.Oktober 23:59 Uhr",
+                "30.Sep 2023"
+            },
+            4,
+            new[]
+            {
+                (new DateTime(2023,10,1,0,0,0), DateUsage.ValidFrom),
+                (new DateTime(2023,10,31,23,59,0), DateUsage.ValidTo),
+                (new DateTime(2023,09,30,0,0,0), DateUsage.SaleDate),
+                (new DateTime(1991,04,25,0,0,0), DateUsage.BDay),
+
+            }
+        };
+        yield return new object[]
+       {
+            new[]
+            {
+                "25.04.1991",
+                "1.Oct 2023 00:00 Uhr bis 31.Oktober 23:59 Uhr",
+                "30.Sep 2023"
+            },
+            4,
+            new[]
+            {
+                (new DateTime(2023,10,1,0,0,0), DateUsage.ValidFrom),
+                (new DateTime(2023,10,31,23,59,0), DateUsage.ValidTo),
+                (new DateTime(2023,09,30,0,0,0), DateUsage.SaleDate),
+                (new DateTime(1991,04,25,0,0,0), DateUsage.BDay),
+
+            }
+       };
     }
 
     IEnumerator IEnumerable.GetEnumerator()
